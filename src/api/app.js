10722d1a2152f8d48761bc8fe3943113ded28f54
minhxiro <94245285+dinhminh0307@users.noreply.github.com>
2024-04-07 11:03:33 +0700

    feat: change the get method

--- src/api/app.js
+++ src/api/app.js
@@ -1,33 +1,29 @@
 
+// module.exports = (req, res) => {
+//     if (req.method === 'POST') {
+//         res.json({ message: 'Signal received successfully!' });
+//     } else if(req.method === 'GET') {
+//         res.status(200).json({ message: 'done' });
+//     }
+//      else {
+//         res.status(405).send('Method Not Allowed');
+//     }
+// };
+
 module.exports = (req, res) => {
     if (req.method === 'POST') {
         res.json({ message: 'Signal received successfully!' });
-    } else if(req.method === 'GET') {
-        res.status(200).json({ message: 'done' });
-    }
-     else {
+    } else if (req.method === 'GET') {
+        // Check if the request has a query parameter 'type' set to 'esp32'
+        if (req.query.type === 'esp32') {
+            // Handle ESP32 polling request
+            res.status(200).json({ message: 'ESP32 polling received.' });
+        } else {
+            // Handle regular browser GET request
+            res.status(200).json({ message: 'done' });
+        }
+    } else {
         res.status(405).send('Method Not Allowed');
     }
 };
-
-// const fetch = require('node-fetch');
-
-// // /api/send-signal.js
-// let signal = false; // This flag should be stored in a database for persistence
-
-// module.exports = (req, res) => {
-//     if (req.method === 'POST') {
-//         signal = true; // Set the flag when the browser button is clicked
-//         return res.json({ message: 'Signal received. Awaiting ESP32 pickup.' });
-//     } else if (req.method === 'GET') {
-//         // ESP32 polls this endpoint
-//         if (signal) {
-//             signal = false; // Reset the flag
-//             return res.json({ signal: true });
-//         } else {
-//             return res.json({ signal: false });
-//         }
-//     }
-//     return res.status(405).send('Method Not Allowed');
-// };
 
