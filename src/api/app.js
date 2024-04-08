c910407bd496934325029d2a4fc56c20e24632f4
minhxiro <94245285+dinhminh0307@users.noreply.github.com>
2024-04-08 14:27:36 +0700

    Update app.js

--- src/api/app.js
+++ src/api/app.js
@@ -18,11 +18,11 @@ module.exports = (req, res) => {
         if (req.query.type === 'esp32') {
             if (signal === 1) {
                 // If the signal is set, send it to the ESP32 and reset the signal
-                res.status(200).json({ signal: 1, message: 'Signal retrieved by ESP32. Signal is now reset.' });
+                res.status.json({ signal: 1, message: 'Signal retrieved by ESP32. Signal is now reset.' });
                 signal = 0; // Reset signal after sending
             } else {
                 // If there is no signal, inform the ESP32 accordingly
-                res.status(200).json({ signal: 0, message: 'No signal for ESP32.' });
+                res.status.json({ signal: 0, message: 'No signal for ESP32.' });
             }
     } else {
         res.status(405).send('Method Not Allowed');
